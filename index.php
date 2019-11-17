8999f788ec02c8685a456a09ce85ec35c1152d7d
Sanja <sanja293@gmail.com>
2019-11-17 15:43:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=C, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Citrus</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="content">
+            <h1 class="title">Catalog</h1>
+            <div class="catalog">
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+                <div class="card">
+                    <img class="image" src="images/img1.jpg" />
+                    <h3 class="title">Title1</h3>
+                    <p class="description"> some description, about something...</p>
+                </div>
+            </div>
+            <h1 class="title">Comments</h1>
+            <div class="comments">
+                <div class="comment">
+                    <div class="header">
+                        <span class="user">damjan</span>
+                        <span class="date">11.11.1999.</span>
+                    </div>
+                    <p class="message">Ja sam bezobrazan</p>
+                </div>
+                <div class="comment">
+                    <div class="header">
+                        <span class="user">damjan</span>
+                        <span class="date">11.11.1999.</span>
+                    </div>
+                    <p class="message">Ja sam bezobrazan</p>
+                </div>
+                <div class="comment">
+                    <div class="header">
+                        <span class="user">damjan</span>
+                        <span class="date">11.11.1999.</span>
+                    </div>
+                    <p class="message">Ja sam bezobrazan</p>
+                </div>
+                <div class="comment">
+                    <div class="header">
+                        <span class="user">damjan</span>
+                        <span class="date">11.11.1999.</span>
+                    </div>
+                    <p class="message">Ja sam bezobrazan</p>
+                </div>
+                <div class="comment">
+                    <div class="header">
+                        <span class="user">damjan</span>
+                        <span class="date">11.11.1999.</span>
+                    </div>
+                    <p class="message">Ja sam bezobrazan</p>
+                </div>
+            </div>
+            <h1 class="title">New Comment</h1>
+            <div class="form">
+                <div class="element">
+                    <label for="name">Name</label>
+                    <input class="input" id="name" type="text" placeholder="name" />
+                </div>
+                <div class="element">
+                    <label for="email">Email</label>
+                    <input class="input" id="email" type="email" placeholder="email" />
+                </div>
+                <div class="element">
+                    <label for="message">Message</label>
+                    <textarea class="input" id="message" rows="10" cols="50" placeholder="message"></textarea>
+                </div>
+                <div class="element">
+                    <input class="" id="btn-submit" type="button" value="Submit" />
+                    <input class="" id="btn-clear" type="button" value="Clear" />
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
